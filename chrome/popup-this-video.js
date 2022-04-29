8e2ca8a59a3cee3b60337768373abc9abb87c5eb
Ronald Nababan <ronsen@startrek.or.id>
2022-04-29 19:33:23 +0700

    update

--- chrome/popup-this-video.js
+++ chrome/popup-this-video.js
@@ -35,291 +35,311 @@ function openAction() {
 	});
 }
 
-function Video(url) {
-	this.url = url.toLowerCase();
-	
-	this.getPopOutURL = function() {
-		if (this.url.indexOf("youtube.com") > 0) {
-			let v = new Youtubecom(url);
-			return v.popOut();
-		} else if (this.url.indexOf("vimeo.com") > 0) {
-			let v = new Vimeo(url);
-			return v.popOut();
-		} else if (this.url.indexOf("vidio.com") > 0) {
-			let v = new Vidio(url);
-			return v.popOut();
-		} else if (this.url.indexOf("youtu.be") > 0) {
-			let v = new Youtube(url);
-			return v.popOut();
-		} else if (this.url.indexOf("gfycat.com") > 0) {
-			let v = new Gfycat(url);
-			return v.popOut();
-		} else if (this.url.indexOf("dailymotion.com") > 0) {
-			let v = new Dailymotion(url);
-			return v.popOut();
-		} else if (this.url.indexOf("metacafe.com") > 0) {
-			let v = new Metacafe(url);
-			return v.popOut();
-		} else if (this.url.indexOf("twitch.tv") > 0) {
-			let v = new Twitch(url);
-			return v.popOut();
-		} else if (this.url.indexOf("facebook.com") > 0) {
-			let v = new Facebook(url);
-			return v.popOut();
-		} else if (this.url.indexOf("d.tube") > 0) {
-			let v = new Dtube(url);
-			return v.popOut();
-		} else if (this.url.indexOf("viddsee.com") > 0) {
-			let v = new Viddsee(url);
-			return v.popOut();
-		} else if (this.url.indexOf("bitchute.com") > 0) {
-			let v = new BitChute(url);
-			return v.popOut();
-		} else if (this.url.indexOf("archive.org") > 0) {
-			let v = new Archive(url);
-			return v.popOut();
-		} else if (this.url.indexOf("liveleak.com") > 0) {
-			let v = new LiveLeak(url);
-			return v.popOut();
-		} else if (this.url.indexOf("nimo.tv") > 0) {
-			let v = new NimoTv(url);
-			return v.popOut();
-		} else {
-			return url;
-		}
-	};
-}
+class Video {
+	constructor(url) {
+		this.url = url.toLowerCase();
 
-function Youtubecom(url) {
-	this.url = url;
-
-	this.getURLParams = function() {
-		let vars = [], hash;
-	    let hashes = this.url.slice(this.url.indexOf('?') + 1).split('&');
-	    for(let i = 0; i < hashes.length; i++) {
-	        hash = hashes[i].split('=');
-	        vars.push(hash[0]);
-	        vars[hash[0]] = hash[1];
-	    }
-	    return vars;
-	};
-
-	this.getVideoID = function() {
-		return this.getURLParams()["v"];
-	};
-
-	this.popOut = function() {
-		let id = this.getVideoID();
-		if (id == undefined) {
-			return 'https://www.youtube.com/';
-		}
-		return 'https://www.youtube.com/embed/'+ id + '?autoplay=1';
-	};
-}
-
-function Youtube(url) {
-	this.url = url;
-
-	this.getVideoID = function() {
-		let regExp = /[^/]+$/;
-		let match = this.url.match(regExp);
-		return match[0];
-	};
-
-	this.popOut = function() {
-		let id = this.getVideoID();
-		if (id == undefined) {
-			return 'https://www.youtube.com/';
-		}
-		return 'https://www.youtube.com/embed/'+ id + '?autoplay=1';
-	};
-}
-
-function Vimeo(url) {
-	this.url = url;
-
-	this.getVideoID = function() {
-		let regExp = /https:\/\/(www\.)?vimeo.com\/(\d+)($|\/)/;
-		let match = this.url.match(regExp);
-		return match[2];
-	};
-
-	this.popOut = function() {
-		let id = this.getVideoID();
-		if (id == undefined) {
-			return 'https://vimeo.com/';
-		}
-		return 'https://player.vimeo.com/video/'+ id + '?autoplay=1';
-	};
-}
-
-function Vidio(url) {
-	this.url = url;
-
-	this.getVideoID = function() {
-		let regExp = /[^/]+$/;
-		let match = this.url.match(regExp);
-		return match[0];
+		this.getPopOutURL = function () {
+			if (this.url.indexOf("youtube.com") > 0) {
+				let v = new Youtubecom(url);
+				return v.popOut();
+			} else if (this.url.indexOf("vimeo.com") > 0) {
+				let v = new Vimeo(url);
+				return v.popOut();
+			} else if (this.url.indexOf("vidio.com") > 0) {
+				let v = new Vidio(url);
+				return v.popOut();
+			} else if (this.url.indexOf("youtu.be") > 0) {
+				let v = new Youtube(url);
+				return v.popOut();
+			} else if (this.url.indexOf("gfycat.com") > 0) {
+				let v = new Gfycat(url);
+				return v.popOut();
+			} else if (this.url.indexOf("dailymotion.com") > 0) {
+				let v = new Dailymotion(url);
+				return v.popOut();
+			} else if (this.url.indexOf("metacafe.com") > 0) {
+				let v = new Metacafe(url);
+				return v.popOut();
+			} else if (this.url.indexOf("twitch.tv") > 0) {
+				let v = new Twitch(url);
+				return v.popOut();
+			} else if (this.url.indexOf("facebook.com") > 0) {
+				let v = new Facebook(url);
+				return v.popOut();
+			} else if (this.url.indexOf("d.tube") > 0) {
+				let v = new Dtube(url);
+				return v.popOut();
+			} else if (this.url.indexOf("viddsee.com") > 0) {
+				let v = new Viddsee(url);
+				return v.popOut();
+			} else if (this.url.indexOf("bitchute.com") > 0) {
+				let v = new BitChute(url);
+				return v.popOut();
+			} else if (this.url.indexOf("archive.org") > 0) {
+				let v = new Archive(url);
+				return v.popOut();
+			} else if (this.url.indexOf("nimo.tv") > 0) {
+				let v = new NimoTv(url);
+				return v.popOut();
+			} else {
+				return url;
+			}
+		};
 	}
-
-	this.popOut = function() {
-		if (this.url.indexOf('/live/') == -1) 
-			return 'https://www.vidio.com/embed/'+ this.getVideoID() +'?autoplay=true&player_only=true&live_chat=false&mute=false';
-		else
-			return 'https://www.vidio.com/live/'+ this.getVideoID() +'/embed?autoplay=true&player_only=true&live_chat=false&mute=false'
-	};
 }
 
-function Gfycat(url) {
-	this.url = url;
-	
-	this.getVideoID = function() {
-		let regExp = /[^/]+$/;
-		let match = this.url.match(regExp);
-		return match[0];
-	};
-	
-	this.popOut = function() {
-		return "https://gfycat.com/ifr/"+ this.getVideoID();
-	};
+class Youtubecom {
+	constructor(url) {
+		this.url = url;
+
+		this.getURLParams = function () {
+			let vars = [], hash;
+			let hashes = this.url.slice(this.url.indexOf('?') + 1).split('&');
+			for (let i = 0; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				vars.push(hash[0]);
+				vars[hash[0]] = hash[1];
+			}
+			return vars;
+		};
+
+		this.getVideoID = function () {
+			return this.getURLParams()["v"];
+		};
+
+		this.popOut = function () {
+			let id = this.getVideoID();
+			
+			return id == undefined ? this.url : 'https://www.youtube.com/embed/' + id + '?autoplay=1';
+		};
+	}
 }
 
+class Youtube {
+	constructor(url) {
+		this.url = url;
 
-function Dailymotion(url) {
-	this.url = url;
-	
-	this.getVideoID = function() {
-		let regExp = /[^/]+$/;
-		let match = this.url.match(regExp);
-		return match[0];
-	};
-	
-	this.popOut = function() {
-		return "https://www.dailymotion.com/embed/video/"+ this.getVideoID() + "?autoPlay=1";
-	};
+		this.getVideoID = function () {
+			let regExp = /[^/]+$/;
+			let match = this.url.match(regExp);
+			return match[0];
+		};
+
+		this.popOut = function () {
+			let id = this.getVideoID();
+
+			return id == undefined ? this.url : 'https://www.youtube.com/embed/' + id + '?autoplay=1';
+		};
+	}
 }
 
-function Metacafe(url) {
-	this.url = url;
-	
-	this.popOut = function() {
-		return url.replace("watch", "embed");
-	};
-}
+class Vimeo {
+	constructor(url) {
+		this.url = url;
 
-function Twitch(url) {
-	this.url = url;
-	
-	this.getVideoID = function() {
-		let regExp = /[^/]+$/;
-		let match = this.url.match(regExp);
-		return match[0];
-	};
-	
-	this.popOut = function() {
-		let id = this.getVideoID();
-		if (id == undefined) {
-			return "https://www.twitch.tv";
-		}
-		return "https://player.twitch.tv/?channel="+ id;
-	};
-}
-
-function Facebook(url) {
-	this.url = url;
-	
-	this.popOut = function() {
-		if (this.url.toLowerCase().indexOf("/videos/") > 0)
-			return "https://www.facebook.com/plugins/video.php?href="+ encodeURI(this.url) +"&autoplay=true";
-		else
+		this.getVideoID = function () {
+			let regExp = /https:\/\/(www\.)?vimeo.com\/(\d+)($|\/)/;
+			let match = this.url.match(regExp);
+			if (match != null)
+				return match[2];
 			return null;
+		};
+
+		this.popOut = function () {
+			let id = this.getVideoID();
+
+			return id == undefined ? this.url : 'https://player.vimeo.com/video/' + id + '?autoplay=1';
+		};
 	}
 }
 
-function Dtube(url) {
-	this.url = url;
+class Vidio {
+	constructor(url) {
+		this.url = url;
 
-	this.getVideoID = function() {
-		let fragments = url.split('/');
-		if (fragments.length == 6)
-			return fragments[4]+"/"+ fragments[5];
-		if (fragments.length == 7)
-			return fragments[5]+"/"+ fragments[6];
-		return null;
-	};
+		this.getVideoID = function () {
+			let regExp = /[^/]+$/;
+			let match = this.url.match(regExp);
+			return match[0];
+		};
 
-	this.popOut = function() {
-		return this.getVideoID() != null ? "https://emb.d.tube/#!/"+ this.getVideoID() : null;
+		this.popOut = function () {
+			if (this.url.indexOf('/live/') == -1)
+				return 'https://www.vidio.com/embed/' + this.getVideoID() + '?autoplay=true&player_only=true&live_chat=false&mute=false';
+
+			else
+				return 'https://www.vidio.com/live/' + this.getVideoID() + '/embed?autoplay=true&player_only=true&live_chat=false&mute=false';
+		};
 	}
 }
 
-function Viddsee(url) {
-	this.url = url;
+class Gfycat {
+	constructor(url) {
+		this.url = url;
 
-	this.getVideoID = function() {
-		let fragments = url.split('/');
-		return fragments[5];
-	};
+		this.getVideoID = function () {
+			let regExp = /[^/]+$/;
+			let match = this.url.match(regExp);
+			return match[0];
+		};
 
-	this.popOut = function() {
-		return this.getVideoID() != null ? "https://www.viddsee.com/player/"+ this.getVideoID() : null;
+		this.popOut = function () {
+			return "https://gfycat.com/ifr/" + this.getVideoID();
+		};
 	}
 }
 
-function BitChute(url) {
-	this.url = url;
 
-	this.getVideoID = function() {
-		let fragments = url.split('/');
-		return fragments[4];
-	}
+class Dailymotion {
+	constructor(url) {
+		this.url = url;
 
-	this.popOut = function() {
-		return this.getVideoID() != null ? "https://www.bitchute.com/embed/"+ this.getVideoID() : null;
-	}
-}
+		this.getVideoID = function () {
+			let regExp = /[^/]+$/;
+			let match = this.url.match(regExp);
+			return match[0];
+		};
 
-function Archive(url) {
-	this.url = url;
-
-	this.getVideoID = function() {
-		let fragments = url.split('/');
-		console.log(fragments[4]);
-		return fragments[4];
-	}
-
-	this.popOut = function() {
-		return this.getVideoID() != null ? "https://archive.org/embed/"+ this.getVideoID() : null;
-	}	
-}
-
-function LiveLeak(url) {
-	this.url = url;
-
-	this.getURLParams = function() {
-		let lets = [], hash;
-	    let hashes = this.url.slice(this.url.indexOf('?') + 1).split('&');
-	    for(let i = 0; i < hashes.length; i++) {
-	        hash = hashes[i].split('=');
-	        lets.push(hash[0]);
-	        lets[hash[0]] = hash[1];
-	    }
-	    return lets;
-	};
-
-	this.getVideoID = function() {
-		return this.getURLParams()["t"];
-	};
-
-	this.popOut = function() {
-		return this.getVideoID() != null ? "https://www.liveleak.com/e/"+ this.getVideoID() : null;
+		this.popOut = function () {
+			return "https://www.dailymotion.com/embed/video/" + this.getVideoID() + "?autoPlay=1";
+		};
 	}
 }
 
-function NimoTv(url) {
-	this.url = url;
-	
-	this.popOut = function() {
-		return url.replace("live", "embed");
-	};
+class Metacafe {
+	constructor(url) {
+		this.url = url;
+
+		this.popOut = function () {
+			return url.replace("watch", "embed");
+		};
+	}
+}
+
+class Twitch {
+	constructor(url) {
+		this.url = url;
+
+		this.getVideoID = function () {
+			if (this.url.indexOf('/videos/') > 0) {
+				let regExp = /[^/]+$/;
+				let match = this.url.match(regExp);
+				if (match != null) {
+					return 'https://player.twitch.tv/?parent=google.com&video=' + match[0];
+				}
+			} else if (this.url.indexOf('/clip/') > 0) {
+				let regExp = /[^/]+$/;
+				let match = this.url.match(regExp);
+				if (match != null) {
+					return 'https://clips.twitch.tv/embed?parent=google.com&clip=' + match[0];
+				}
+			} else {
+				let regExp = /[^/]+$/;
+				let match = this.url.match(regExp);
+				if (match != null) {
+					return 'https://player.twitch.tv/?parent=google.com&channel=' + match[0];
+				}
+			}
+
+			return null;
+		};
+
+		this.popOut = function () {
+			let id = this.getVideoID();
+			if (id == undefined) {
+				return "https://www.twitch.tv";
+			}
+			return id;
+		};
+	}
+}
+
+class Facebook {
+	constructor(url) {
+		this.url = url;
+
+		this.popOut = function () {
+			if (this.url.toLowerCase().indexOf("/videos/") > 0)
+				return "https://www.facebook.com/plugins/video.php?href=" + encodeURI(this.url) + "&autoplay=true";
+
+			else
+				return this.url;
+		};
+	}
+}
+
+class Dtube {
+	constructor(url) {
+		this.url = url;
+
+		this.getVideoID = function () {
+			let fragments = url.split('/');
+			if (fragments.length == 6)
+				return fragments[4] + "/" + fragments[5];
+			if (fragments.length == 7)
+				return fragments[5] + "/" + fragments[6];
+			return null;
+		};
+
+		this.popOut = function () {
+			return this.getVideoID() != null ? "https://emb.d.tube/#!/" + this.getVideoID() : null;
+		};
+	}
+}
+
+class Viddsee {
+	constructor(url) {
+		this.url = url;
+
+		this.getVideoID = function () {
+			let fragments = url.split('/');
+			return fragments[5];
+		};
+
+		this.popOut = function () {
+			return this.getVideoID() != null ? "https://www.viddsee.com/player/" + this.getVideoID() : null;
+		};
+	}
+}
+
+class BitChute {
+	constructor(url) {
+		this.url = url;
+
+		this.getVideoID = function () {
+			let fragments = url.split('/');
+			return fragments[4];
+		};
+
+		this.popOut = function () {
+			return this.getVideoID() != null ? "https://www.bitchute.com/embed/" + this.getVideoID() : null;
+		};
+	}
+}
+
+class Archive {
+	constructor(url) {
+		this.url = url;
+
+		this.getVideoID = function () {
+			let fragments = url.split('/');
+			console.log(fragments[4]);
+			return fragments[4];
+		};
+
+		this.popOut = function () {
+			return this.getVideoID() != null ? "https://archive.org/embed/" + this.getVideoID() : null;
+		};
+	}
+}
+
+class NimoTv {
+	constructor(url) {
+		this.url = url;
+
+		this.popOut = function () {
+			return url.replace("live", "embed");
+		};
+	}
 }
