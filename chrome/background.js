3c66b5e3a26d0a028b8e27dbe276862091bfadd0
Ronsen <ronsen@startrek.or.id>
2023-05-29 21:05:37 +0700

    update

--- chrome/background.js
+++ chrome/background.js
@@ -52,22 +52,23 @@ class YouTube {
 
     getPopupUrl() {
         let newUrl = this.url.href;
+        let videoId = '';
 
         if (this.url.href.indexOf("youtube.com") > 0) {
             if (this.url.href.indexOf("/shorts/") > 0) {
-                const videoId = this.url.pathname.substring(this.url.pathname.lastIndexOf('/')+ 1,
+                videoId = this.url.pathname.substring(this.url.pathname.lastIndexOf('/') + 1,
                     url.pathname.length);
-                newUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
             } else {
                 const params = new URLSearchParams(this.url.search);
-                if (params.get('v')) {
-                    newUrl = `https://www.youtube.com/embed/${params.get('v')}?autoplay=1`;
-                }
+                videoId = params.get('v');
             }
         }
 
         if (this.url.href.indexOf("youtu.be") > 0) {
-            const videoId = this.url.pathname.substring(1, this.url.pathname.length);
+            videoId = this.url.pathname.substring(1, this.url.pathname.length);
+        }
+
+        if (videoId.length > 0) {
             newUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
         }
 
