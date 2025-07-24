89d5623ba5772e9a3040e71dc7f179abde61d391
Ronsen <ronsen@startrek.or.id>
2025-07-24 13:33:16 +0700

    refactor

--- firefox/background.js
+++ firefox/background.js
@@ -25,48 +25,26 @@ browser.browserAction.onClicked.addListener(openAction);
 function popupThis(url) {
 	let popupUrl = url.href;
 
-	if (url.href.indexOf("youtube.com") > 0 || url.href.indexOf("youtu.be") > 0) {
-		popupUrl = youtube(url);
-	}
+	const handlers = {
+		"youtube.com": youtube,
+		"youtu.be": youtube,
+		"vimeo.com": vimeo,
+		"twitch.tv": twitch,
+		"dailymotion.com": dailymotion,
+		"viddsee.com": viddsee,
+		"bilibili.com": bilibili,
+		"odysee.com": odysee,
+		"bitchute.com": bitchute,
+		"facebook.com": facebook,
+		"kick.com": kick,
+		"vidio.com": vidio
+	};
 
-	if (url.href.indexOf("vimeo.com") > 0) {
-		popupUrl = vimeo(url);
-	}
-
-	if (url.href.indexOf("twitch.tv") > 0) {
-		popupUrl = twitch(url);
-	}
-
-	if (url.href.indexOf("dailymotion.com") > 0) {
-		popupUrl = dailymotion(url);
-	}
-
-	if (url.href.indexOf("viddsee.com") > 0) {
-		popupUrl = viddsee(url);
-	}
-
-	if (url.href.indexOf("bilibili.com") > 0) {
-		popupUrl = bilibili(url);
-	}
-
-	if (url.href.indexOf("odysee.com") > 0) {
-		popupUrl = odysee(url);
-	}
-
-	if (url.href.indexOf("bitchute.com") > 0) {
-		popupUrl = bitchute(url);
-	}
-
-	if (url.href.indexOf("facebook.com") > 0) {
-		popupUrl = facebook(url);
-	}
-
-	if (url.href.indexOf("kick.com") > 0) {
-		popupUrl = kick(url);
-	}
-
-	if (url.href.indexOf("vidio.com") > 0) {
-		popupUrl = vidio(url);
+	for (const domain in handlers) {
+		if (url.href.includes(domain)) {
+			popupUrl = handlers[domain](url);
+			break;
+		}
 	}
 
 	chrome.windows.create({
